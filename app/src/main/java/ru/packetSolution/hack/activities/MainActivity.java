62b659dd5bc6f3877cd073729ff4f8aca4190e07
admin <mariavasina@inbox.ru>
2023-04-11 20:31:28 +0300

    SearchView (cripple)

--- app/src/main/java/ru/packetSolution/hack/activities/MainActivity.java
+++ app/src/main/java/ru/packetSolution/hack/activities/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.SearchView;
 
 import com.google.android.material.navigation.NavigationBarView;
 
